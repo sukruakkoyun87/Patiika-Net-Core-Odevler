7250a6bcc95608b5db433bab9adc2c6b2467ceb2
şükrü akkoyun <Sukruakkoyun87@hotmail.com>
2022-03-01 00:33:20 +0300

    Net Core | If Else ve Ternary If

---
+++ Net-Core-If-Else/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//  If - If  Else - Else If
+
+int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11)
+{
+    Console.WriteLine("Günaydın  :)");
+}
+else if (time <= 18)
+{
+    Console.WriteLine(" İyi günler :)");
+}
+else
+{
+    Console.WriteLine("İyi akşamlar :)");
+}
+
+
+// Ternary If
+
+string message=time>=6 && time< 11?"Günaydın :)":time<=18?"İyi günler :)":"İyi akşamlar :)";
+
+Console.WriteLine(message);
+
+
