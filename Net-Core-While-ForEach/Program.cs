2ceee9e05b8fe82624268d0283a8810e2eefea75
şükrü akkoyun <Sukruakkoyun87@hotmail.com>
2022-03-01 03:14:16 +0300

    Net Core | Foreach ve while dongusu

---
+++ Net-Core-While-ForEach/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ // While Dongusu
+
+ // 1 den başlayarak  consoledan girilen sayı dahil ortlama hesaplama
+
+ Console.Write("Sayı giriniz: ");
+ int sayi=Convert.ToInt32(Console.ReadLine());
+
+ int sayac=1;
+ int toplam=0;
+
+while (sayac<=sayi)
+{
+    toplam += sayac;
+    sayac++;
+}
+
+Console.WriteLine("Ortalama: " + toplam/sayi);
+
+
+// a dsn z ye kadar tum harfleri yazdırma
+
+char character='a';
+
+while (character<='z')
+{
+    Console.Write(character +" ");
+    character++;
+
+}
+
+Console.WriteLine();
+
+// Foreach dongusu
+
+string[] arabalar={"Mercedes","BMW","Audi","Ferrari","Porsche"};
+
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
